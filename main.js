248c006d9d1e508937150c06889972c22975e446
Nono Martínez Alonso <mundowarezweb@gmail.com>
2018-02-24 09:33:21 -0500

    Add drawing app template

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-// Live reload module which watches `public` folder
+const { app, BrowserWindow } = require('electron')
+    // Live reload module which watches `public` folder
 const _ = require('electron-reload')(__dirname + '/public')
 
 const path = require('path')
@@ -13,34 +13,34 @@ let mainWindow
 // A function to create the browser window when the app is ready
 function createWindow() {
 
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    // center: true,
-    // frame: false, 
-    // resizable: false,
-    // alwaysOnTop: true,
-    useContentSize: true    // when false, width/height will set the size of the whole app, including frames. If true, innerWindow will be set instead, resulting in a bigger app window
-  })
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        // center: true,
+        // frame: false, 
+        // resizable: false,
+        // alwaysOnTop: true,
+        useContentSize: true // when false, width/height will set the size of the whole app, including frames. If true, innerWindow will be set instead, resulting in a bigger app window
+    })
 
-  // Load the index.html of the app
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'public', 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+    // Load the index.html of the app
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'public', 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
 
-  // Open the DevTools on start
-  // mainWindow.webContents.openDevTools("undock")
+    // Open the DevTools on start
+    // mainWindow.webContents.openDevTools("undock")
 
-  // Emitted when the window is closed
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element
-    mainWindow = null
-  })
+    // Emitted when the window is closed
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element
+        mainWindow = null
+    })
 }
 
 // This method will be called when Electron has finished
@@ -50,19 +50,19 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', function() {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
 })
 
 // In this file you can include the rest of your app's specific main process
